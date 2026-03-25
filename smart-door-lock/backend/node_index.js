1a439aa33b9dc606b3c621f65e1e57fb0e84622c
Sanket-Kawale <sanketkawale101@gmail.com>
2026-03-26 00:18:59 +0530

    Update node_index.js

--- smart-door-lock/backend/node_index.js
+++ smart-door-lock/backend/node_index.js
@@ -1,3 +1,9 @@
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log("Server running...");
+});
+
 const express = require("express");
 const cors = require("cors");
 const pool = require("./db");
